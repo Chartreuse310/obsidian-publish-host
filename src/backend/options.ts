996dc7baef4ee3529079dd12a2c98ca4f319ad5f
jooooock <findsource@proton.me>
2024-03-06 18:00:40 +0800

    update

--- src/backend/options.ts
+++ src/backend/options.ts
@@ -1,11 +1,12 @@
 export default {
-    "siteName": "Publish Demo",
-    "indexFile": "Home",
-    "defaultTheme": "light",
-    "showThemeToggle": true,
-    "showHoverPreview": true,
-    "strictLineBreaks": true,
-    "showSearch": true,
-    "showOutline": true,
-    "showBacklinks": true
+    siteName: "Publish Demo",
+    indexFile: "Home",
+    defaultTheme: "light",
+    showThemeToggle: true,
+    showHoverPreview: true,
+    strictLineBreaks: true,
+    showSearch: true,
+    showOutline: true,
+    showBacklinks: true,
+    showGraph: false,
 }
